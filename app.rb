3896afc4da6b87a76bfea45506748a9c3864482d
andy jenkins <andygjenkins@gmail.com>
2016-10-10 14:24:12 +0100

    Add different route

--- app.rb
+++ app.rb
@@ -1,5 +1,15 @@
 require 'sinatra'
+require 'shotgun'
 
-get '/' do
-  'Hello!'
-end
+
+  get '/' do
+    'Hello!'
+  end
+
+  get '/secret' do
+    'This is a secret'
+  end
+
+  get '/supersecret' do
+    'This is the most secret'
+  end
