15d63fd2690a6ce96e566957b87052eeaa3d1c00
James <bakerje@mac.com>
2016-10-10 14:54:10 +0100

    Add cat impage and red dashed line

--- app.rb
+++ app.rb
@@ -1,6 +1,6 @@
 require 'sinatra'
 require 'shotgun'
-
+set :session_secret, 'super secret', 'very secret'
 
   get '/' do
     'Hello!'
@@ -11,5 +11,15 @@ require 'shotgun'
   end
 
   get '/supersecret' do
-    'This is the most secret'
+    'This is now the most secret'
+  end
+
+  get '/verysecret' do
+    'The most secret of all'
+  end
+
+  get '/cat' do
+    "<div style='border: 3px dashed red'>
+      <img src='http://bit.ly/1eze8aE'>
+    </div>"
   end
